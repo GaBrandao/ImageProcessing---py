4a684b588ada0a110b7d675c794967f2598f555e
Gabriel Brandão <gabriel.brandaoal@gmail.com>
2019-03-10 17:10:46 -0300

    Add assert message for arguments

--- imageToBinary.py
+++ imageToBinary.py
@@ -1,24 +1,27 @@
-from sys import argv
+import sys
 from PIL import Image as im
 from PIL import ImageEnhance as ie
 from PIL import ImageOps as io
 
-if (len(argv)<4):
-    print("This program ")
+if (len(sys.argv)<4):
+    print("To execute use:\n\t python3 imageToBinary.py <image-filename> <contrast-percent> <output-filename> <dimension-OTPIONAL>")
+    sys.exit(0)
 
-
-image = im.open(argv[1])
+image = im.open(sys.argv[1])
 
 size = (128, 128)
+if (len(sys.argv)==5):
+    d = int(sys.argv[4])
+    size = (d,d)
 if (image.size!=size):
     image = io.fit(image,size,im.ANTIALIAS)
 
 image = io.grayscale(io.equalize(image))
-image = ie.Contrast(image).enhance(float(argv[2])).convert("1")
+image = ie.Contrast(image).enhance(float(sys.argv[2])).convert("1")
 
 list_image = io.mirror(image).rotate(90).load()
 
-out = open(argv[3], "w+")
+out = open(sys.argv[3], "w+")
 out.write(str(size[0])+"\n")
 for i in range(size[0]):
     for j in range(size[1]):
